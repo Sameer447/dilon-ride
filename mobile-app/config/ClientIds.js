4bd3b61de414d82fef146068675118fdbe9ecf07
Sameer Shoukat <sameershoukat526@gmail.com>
2025-09-27 13:12:22 +0500

    WIP

--- mobile-app/config/ClientIds.js
+++ mobile-app/config/ClientIds.js
@@ -2,5 +2,5 @@
 export default {
   webClientId: "853981952349-64f365ibu2k3k2q7nupr25p1of14odjl.apps.googleusercontent.com",
   iosClientId: "853981952349-ios_client_id.apps.googleusercontent.com",
-  androidClientId: "853981952349-android_client_id.apps.googleusercontent.com",
+  androidClientId: "853981952349-6202u1ige55cm9a56ds1pa924cjfv76i.apps.googleusercontent.com",
 };
